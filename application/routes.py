2c70c6fc7f509ed8f53a7efb146135b60da68b61
Likith <likithgowda1265@gmail.com>
2023-04-04 00:57:46 +0530

    search page along with sign up page is compleated
    work is going on...

--- application/routes.py
+++ application/routes.py
@@ -3,5 +3,22 @@ from flask import render_template,request,redirect
 
 @app.route("/")
 def welcome():
+    title="index"
+    return render_template("index.html",title=title)
+
+@app.route("/login")
+def login():
+
     title="login"
-    return render_template("loginpage.html",title=title)
+    return render_template("login.html",title=title)
+@app.route("/signup")
+def signup():
+
+    title="signup"
+    return render_template("signup.html",title=title)
+
+@app.route("/search")
+def search():
+
+    title="search"
+    return render_template("search.html",title=title)

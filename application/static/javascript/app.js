b2596e073af2e518db2634daba382c0d7cbbdd3d
Likith <lavinelev@gmail.com>
2023-04-12 22:05:09 +0530

    updates

--- application/static/javascript/app.js
+++ application/static/javascript/app.js
@@ -1,15 +1,1 @@
-const dataPie = {
-    type: "pie",
-    data: {
-      labels: ["Monday", "Tuesday", "Wednesday", "Thursday", "lo"],
-      datasets: [{
-        data: [1234, 2234, 3234, 4234, 0],
-        backgroundColor: ["rgba(117,169,255,0.6)", "rgba(28,28,28,0.6)",
-          "rgba(208,129,222,0.6)", "rgba(247,127,167,0.6)",
-          "rgba(66,133,244,0.6)"
-        ],
-      }, ],
-    },
-  };
-  
-  new mdb.Chart(document.getElementById("chart-pie"), dataPie);
+document.write("hello world");

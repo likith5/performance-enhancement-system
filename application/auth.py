7f3e7b48ede20ff330ee1177bcfb9e426fda1b36
Likith <lavinelev@gmail.com>
2023-06-05 09:22:55 +0530

    push it

--- application/auth.py
+++ application/auth.py
@@ -14,14 +14,29 @@ def login():
         email = request.form['email']
         password = request.form['password']
         user = db.users.find_one({'email': email})
+        teacher = db.teacher.find_one({'email': email})
         if user and check_password_hash(user['password'], password):
+            
+            
+            marks = user.get('marks')
+            if marks is not None :
+                flash('Logged in successfully.', category='success')
+                session["studentemail"] = email
+
+                # return render_template('dashboard.html')
+                return redirect(url_for('views.dashboard'))
+            else:
+                flash('marks has not been given yet',category='error')
+                return redirect(url_for('auth.login'))
+
+        if teacher and check_password_hash(teacher['password'], password):
             flash('Logged in successfully.', category='success')
             
-            session["email"] = email
+            session["teacheremail"] = email
 
 
-            # return render_template('index.html')
-            return redirect(url_for('views.dashboard'))
+            # return render_template('search.html')
+            return redirect(url_for('views.search'))
         else:
             flash('Invalid username or password.',category='error')
             return redirect(url_for('auth.login'))
@@ -36,6 +51,8 @@ def sign_up():
     if request.method == 'POST':
         email = request.form.get('email')
         username = request.form.get('username')
+        role = request.form.get('role')
+
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
         hashed_password = bcrypt.hashpw(password1.encode('utf-8'), bcrypt.gensalt())
@@ -52,10 +69,16 @@ def sign_up():
             flash('Password must be at least 7 characters.', category='error')
         elif db.users.find_one({'email': email}) :
             flash('Username already exists.',  category='error')
-        else:
+        elif role == 'student':
             hashed_password = generate_password_hash(password1)
 
-            db.users.insert_one({'email':email,'username': username, 'password':  hashed_password})
+            db.users.insert_one({'email':email,'role':role,'username': username, 'password':  hashed_password})
+            flash('Account created!',category='success')
+            return redirect(url_for('auth.login'))
+        elif role == 'teacher':
+            hashed_password = generate_password_hash(password1)
+
+            db.teacher.insert_one({'email':email,'username': username, 'password':  hashed_password})
             flash('Account created!',category='success')
             return redirect(url_for('auth.login'))
        
@@ -63,7 +86,9 @@ def sign_up():
     
 @auth.route('/logout')
 def logout():
-    session.pop("email", None) 
+    session.pop("studentname",None) 
+    session.pop("studentemail",None) 
+    session.pop("teacheremail", None) 
     return redirect(url_for('auth.login'))   
 
    
--- application/views.py
+++ application/views.py
@@ -1,7 +1,8 @@
 from application import create_app
-from flask import Blueprint,url_for,session
+from flask import Blueprint,url_for,session,flash
 from flask_session import Session
-
+from application import db
+from pymongo.errors import PyMongoError
 from flask import render_template,request,redirect
 views = Blueprint('views',__name__)
 
@@ -22,14 +23,211 @@ def profile():
     title="profile"
     return render_template("profile.html",title=title)
 
-@views.route("/dashboard")
-def dashboard():
-    if 'email' in session:
-         name = session.get("email")
-         title="dashboard"
-         return render_template('dashboard.html',title=title,name=name)
+@views.route("/marksenter",methods=['GET','POST'])
+def marksenter():
+    
+    studentname = session.get('studentname')
+    title="marks enter"
+    if  'teacheremail' in session:
+
+        if request.method == 'POST':
+
+                communication= request.form.get('communication')
+                technical= request.form.get('technical')
+                creativity= request.form.get('creativity')
+                projectmmt= request.form.get('projectmmt')
+                timemanagement= request.form.get('timemanagement')
+                generalknowledge= request.form.get('generalknowledge')
+                interpersonal= request.form.get('interpersonal')
+                resultoriented= request.form.get('resultoriented')
+                leardership= request.form.get('leardership')
+                presentation= request.form.get('presentation')
+                # print(leardership)
+                # print(presentation)
+                # print(studentname)
+                # print(communication)
+
+                # user = db.users.find_one({'username':username })
+                # email = user['email']
+                # db.marks.insert_one({"communication": communication})
+                try:
+
+                    db.users.update_one(
+                    {"username": studentname},
+                    {
+                        "$set": {
+                            "marks": {
+                                "communication": communication,
+                                "technical": technical,
+                                "creativity": creativity,
+                                "projectmmt": projectmmt,
+                                "timemanagement": timemanagement,
+                                "generalknowledge": generalknowledge,
+                                "interpersonal": interpersonal,
+                                "resultoriented": resultoriented,
+                                "leardership": leardership,
+                                "presentation": presentation
+                            }
+                        }
+                    }
+                )
+                    flash('Marks entered successfully', category='success')
+                    return redirect(url_for('views.dashboard'))
+                except PyMongoError as e:
+                    flash(f'Error: {str(e)}', category='error')
+                # Handle the error accordingly, such as logging it or displaying an error message to the user
+                    return redirect(url_for('views.marksenter'))
+                
+        
+        # print(studentname)
+        # return render_template("marksenter.html",title=title,studentname=studentname)
+        return render_template("marksenter.html")
     return redirect(url_for('auth.login'))
 
+
+@views.route("/dashboard")
+def dashboard():
+    if 'studentemail' in session :
+        def converttorange(grade):
+            if grade == 1:
+                percentage = 20
+            elif grade == 2:
+                percentage = 40
+            elif grade == 3:
+                percentage = 60
+            elif grade == 4:
+                percentage = 80
+            elif grade == 5:
+                percentage = 100
+            else:
+                percentage = 29
+            
+           
+            return percentage
+        
+        name = session.get("studentemail")
+        print(name)
+        user = db.users.find_one({'email':name })
+        studname = user['username']
+        communication = int(user['marks'].get('communication'))
+        technical = int(user["marks"].get('technical'))
+        creativity = int(user['marks'].get('creativity'))
+        projectmm = int(user['marks'].get('projectmmt'))
+        timemanagement = int(user['marks'].get('timemanagement'))
+        generalknowledge = int(user['marks'].get('generalknowledge'))
+        interpersonal = int(user['marks'].get('interpersonal'))
+        resultoriented = int(user['marks'].get('resultoriented'))
+        leardership = int(user['marks'].get('leardership'))
+        presentation = int(user['marks'].get('presentation'))
+
+        communication = converttorange(communication)
+        creativity = converttorange(creativity)
+        technical = converttorange(technical)
+        projectmm = converttorange(projectmm)
+        timemanagement = converttorange(timemanagement)
+        generalknowledge = converttorange(generalknowledge)
+        interpersonal = converttorange(interpersonal)
+        resultoriented = converttorange(resultoriented)
+        leardership = converttorange(leardership)
+        presentation = converttorange(presentation)
+        
+
+
+        
+      
+
+
+        title="dashboard"
+        return render_template('dashboard.html',title=title,studname=studname,creativity=creativity,communication=communication,technical=technical,projectmm=projectmm,timemanagement=timemanagement,generalknowledge=generalknowledge,interpersonal=interpersonal,resultoriented=resultoriented,leardership=leardership,presentation=presentation)
+    # return redirect(url_for('auth.login'))
+    elif  'teacheremail' in session :
+        def converttorange(grade):
+            if grade == 1:
+                percentage = 20
+            elif grade == 2:
+                percentage = 40
+            elif grade == 3:
+                percentage = 60
+            elif grade == 4:
+                percentage = 80
+            elif grade == 5:
+                percentage = 100
+            else:
+                percentage = None
+            
+           
+            return percentage
+        
+        studentname = session.get("studentname")
+        studname=studentname
+        print(studentname)
+        user = db.users.find_one({'username':studentname })
+        # communicat = user['marks'].get('communication')
+        communication = int(user['marks'].get('communication'))
+        technical = int(user["marks"].get('technical'))
+        creativity = int(user['marks'].get('creativity'))
+        projectmm = int(user['marks'].get('projectmmt'))
+        timemanagement = int(user['marks'].get('timemanagement'))
+        generalknowledge = int(user['marks'].get('generalknowledge'))
+        interpersonal = int(user['marks'].get('interpersonal'))
+        resultoriented = int(user['marks'].get('resultoriented'))
+        leardership = int(user['marks'].get('leardership'))
+        presentation = int(user['marks'].get('presentation'))
+
+        communication = converttorange(communication)
+        creativity = converttorange(creativity)
+        technical = converttorange(technical)
+        projectmm = converttorange(projectmm)
+        timemanagement = converttorange(timemanagement)
+        generalknowledge = converttorange(generalknowledge)
+        interpersonal = converttorange(interpersonal)
+        resultoriented = converttorange(resultoriented)
+        leardership = converttorange(leardership)
+        presentation = converttorange(presentation)
+        
+
+
+        
+      
+
+
+        title="dashboard"
+        return render_template('dashboard.html',studname=studname,title=title,creativity=creativity,communication=communication,technical=technical,projectmm=projectmm,timemanagement=timemanagement,generalknowledge=generalknowledge,interpersonal=interpersonal,resultoriented=resultoriented,leardership=leardership,presentation=presentation)
+    return redirect(url_for('auth.login'))
+
+@views.route("/search",methods=['GET','POST'])
+def search():
+    if 'teacheremail' in session:
+        username = request.form.get('username')
+        usn = request.form.get('usn')
+
+        if request.method == 'POST':
+            college = request.form.get('college')
+            username = request.form.get('username')
+            usn = request.form.get('usn')
+        
+            accademicyear = request.form.get('accademicyear')
+            user = db.users.find_one({'username':username })
+            # user = db.users.find_one({'email':name })
+            # studname = user['username']
+
+            # print(email)
+            if user:
+                session["studentname"] = username
+                # bro = session.get['studentname']
+                flash('User found Kindly enter the marks ', category='success')
+                return redirect(url_for('views.marksenter'))
+                # return render_template('marksenter.html',usn=usn,usernames=username)
+            else:
+                flash('User not found', category='error')
+                return redirect(url_for('views.search'))
+                # return render_template('dashboard.html',title=title,name=name)   
+        return render_template('search.html',usn=usn,username=username)
+    return redirect(url_for('auth.login'))
+
+    
+       
+        
 
 
 
